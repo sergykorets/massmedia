2a2e4cac6ac899de5571f0ed7fd51807115e6b83
gokart <sergykoretsfsp@gmail.com>
2017-06-15 22:48:23 +0300

    jquery migrate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
+//= require jquery-migrate-min
 //= require selectize
 //= require bootstrap-sprockets
 //= require_tree .
